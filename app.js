e6e2b2360defa5a4e3928076097aa50039865f2c
Franco Panozzo <francopanozzo@hotmail.com.ar>
2021-09-24 19:04:00 +0200

    Express app basic setup and env variables usage

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+// Load config
+dotenv.config({ path: './config/.env' })
+
+const PORT = process.env.PORT || 3000
+
+// App init
+const app = express()
+
+app.listen(PORT, console.log(`Running in ${process.env.NODE_ENV.toUpperCase()} mode on port: ${PORT}.`))
